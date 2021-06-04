81cabdddd1b0d44b1afe4e466d0947e3d57b5a96
Barbaro <barbarodelarosa@gmail.com>
2021-06-04 02:10:54 -0400

    Server funcionando cosas por arreglar 1-030621

--- database/config.database.js
+++ database/config.database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.MONDODB_CNN_DEV, {
+        await mongoose.connect(process.env.MONDODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
